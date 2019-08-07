1fc8c9766684c0f2847fda9fc30f7e5aa814fc1c
fan <fan.hu@msn.com>
2019-08-07 10:28:43 +0800

    sort

--- src/JavaScript/this.ts
+++ src/JavaScript/this.ts
@@ -19,3 +19,12 @@ var f = foo.call({ id: 1 });
 var f1 = f.call({ id: 2 })()();
 var f2 = f().call({ id: 3 })();
 var f3 = f()().call({ id: 4 });
+
+let para: any = {};
+let children = para && para.go && para.go[0] ? { cc: 23 } : { cc: "null" };
+console.log("children", children);
+
+let numberA: number[] = [33, 21, 45, 43, 53, 56, 67, 997];
+let ccv = numberA.map((data, index) => [index, data]);
+let cx = ccv.sort((f, b) => f[1] - b[1]);
+console.log(cx);
